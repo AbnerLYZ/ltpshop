fc788beb5f563511f37f7804c9ac2952d06bf55d
AbnerLYZ <Abner3721@163.com>
2017-05-08 16:01:05 +0800

    second submission
    添加内容

--- demo.php
+++ demo.php
@@ -1,17 +1,4 @@
 <?php
-// 检测PHP环境
-if(version_compare(PHP_VERSION,'5.4.0','<'))  die('require PHP > 5.4.0 !');
-
-//检测是否已安装TPshop系统
-if(file_exists("./install/") && !file_exists("./install/install.lock")){
-	if($_SERVER['PHP_SELF'] != '/index.php'){
-		header("Content-type: text/html; charset=utf-8");         
-		exit("请在域名根目录下安装,如:<br/> www.xxx.com/index.php 正确 <br/>  www.xxx.com/www/index.php 错误,域名后面不能圈套目录, 但项目没有根目录存放限制,可以放在任意目录,apache虚拟主机配置一下即可");
-	}  
-	header('Location:/install/index.php');
-	exit(); 
-	
-}
-
+echo "github学习测试中";
 
 ?>
